925237b21a2788aafe53bb5d6ff8368306dc0428
slfiene <sean.fiene@nintex.com>
2016-11-07 15:35:15 -0800

    asdf

---
+++ pir.js
@@ -0,0 +1,15 @@
+var Gpio = require('onoff').Gpio,
+  sensor = new Gpio(17, 'in', 'both');    //#A
+
+sensor.watch(function (err, value) { //#B
+  if (err) exit(err);
+  console.log(value ? 'there is someone!' : 'not anymore!');
+});
+
+function exit(err) {
+  if (err) console.log('An error occurred: ' + err);
+  sensor.unexport();
+  console.log('Bye, bye!')
+  process.exit();
+}
+process.on('SIGINT', exit);
